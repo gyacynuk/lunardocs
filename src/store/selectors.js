76ebfe92b0564d878cc3f7ba6ff5a47f4b5d246c
Griffin <gyacynuk@gmail.com>
2020-10-15 01:08:32 -0400

    Remove redux example code

--- src/store/selectors.js
+++ src/store/selectors.js
@@ -19,32 +19,3 @@ export const getActiveDocumentValue = store => store.editor.activeDocument.value
 export const getShortcutTarget = store => store.editor.shortcut.target;
 export const getShortcutSearch = store => store.editor.shortcut.search;
 export const getShortcutDropdownIndex = store => store.editor.shortcut.dropdownIndex;
-
-// Examples
-export const getTodosState = store => store.todos;
-
-export const getTodoList = store =>
-  getTodosState(store) ? getTodosState(store).allIds : [];
-
-export const getTodoById = (store, id) =>
-  getTodosState(store) ? { ...getTodosState(store).byIds[id], id } : {};
-
-/**
- * example of a slightly more complex selector
- * select from store combining information from multiple reducers
- */
-export const getTodos = store =>
-  getTodoList(store).map(id => getTodoById(store, id));
-
-export const getTodosByVisibilityFilter = (store, visibilityFilter) => {
-  const allTodos = getTodos(store);
-  switch (visibilityFilter) {
-    case VISIBILITY_FILTERS.COMPLETED:
-      return allTodos.filter(todo => todo.completed);
-    case VISIBILITY_FILTERS.INCOMPLETE:
-      return allTodos.filter(todo => !todo.completed);
-    case VISIBILITY_FILTERS.ALL:
-    default:
-      return allTodos;
-  }
-};
